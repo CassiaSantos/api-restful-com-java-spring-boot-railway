0bf89e34b77f1eead8b813a730e7e80964bd2f48
Cássia Oliveira dos Santos <86209186+CassiaSantos@users.noreply.github.com>
2024-07-07 20:05:39 -0300

    feat: arquivos iniciais do projeto

---
+++ src/test/java/com/santos/cassia/api_rest_com_spring_boot_java_railway/ApiRestComSpringBootJavaRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.cassia.api_rest_com_spring_boot_java_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestComSpringBootJavaRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
