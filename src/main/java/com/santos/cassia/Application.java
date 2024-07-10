3f9d5e156b39f9392c333276f91a2440cce0e634
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-07-09 21:35:42 -0300

    feat: implementa detalhes finais do projeto

---
+++ src/main/java/com/santos/cassia/Application.java
@@ -0,0 +1,16 @@
+package com.santos.cassia.api_rest_com_spring_boot_java_railway;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
